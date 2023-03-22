e0036d6da39e29529d5009ade9cef8dc3395930f
Eduardo <edugramosf@gmail.com>
2023-03-22 17:37:56 -0300

    Update 0.2

--- ControleDeContatos/Program.cs
+++ ControleDeContatos/Program.cs
@@ -1,9 +1,13 @@
+using ControleDeContatos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
