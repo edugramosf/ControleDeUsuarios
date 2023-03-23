d992b705c80dcb3891288864aeec89591e1e8366
Eduardo <edugramosf@gmail.com>
2023-03-23 17:41:51 -0300

    Update 0.3

--- ControleDeContatos/Data/AppDbContext.cs
+++ ControleDeContatos/Data/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ControleDeContatos.Data
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer("DataSource=app.db;Cache=Shared");
+            
     }
 }
--- ControleDeContatos/Program.cs
+++ ControleDeContatos/Program.cs
@@ -1,9 +1,10 @@
 using ControleDeContatos.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
